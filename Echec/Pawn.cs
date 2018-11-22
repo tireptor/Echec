01aa9ab271208c3e0b9ebdcce7f1f8365a0bedcf
Lafargue Vincent <vincentdu76230@gmail.com>
2018-11-22 21:51:24 +0100

    Modif

---
+++ Echec/Pawn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Echec
+{
+    // Classe du pion
+    class Pawn : Pieces
+    {
+    }
+}
